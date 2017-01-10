abb800e9426797dfa686baff82f21e9f860276c2
jpoullet2000 <jeanbaptistepoullet@gmail.com>
2017-01-10 22:35:11 +0100

    correction in tests

--- tests/test_custom2dshapes.py
+++ tests/test_custom2dshapes.py
@@ -6,7 +6,7 @@ class TestCustom2dShapes(unittest.TestCase):
 
     def test_regular_polygon(self):
         triangle = c2d.regular_polygon(3, 10)
-        self.assertTrue(c.dumps().startswith("polygon(points=[[10.0, 0.0], [-4.99"))
+        self.assertTrue(triangle.dumps().startswith("polygon(points=[[10.0, 0.0], [-4.99"))
 
     def test_star(self):
         c = c2d.star(20, [6, 10])
