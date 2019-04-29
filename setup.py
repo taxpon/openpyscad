affc8bf6bdfb1197f44023c579b5188e89503177
Takuro Wada <taxpon@gmail.com>
2019-04-29 23:24:17 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import find_packages
 
 import openpyscad
 
-with open("README.rst", "r") as fh:
+with open('README.rst', 'r') as fh:
     long_description = fh.read()
+
+with open('requirements.txt', 'r') as fh:
+    requirements = fh.readlines()
 
 
 setup(
@@ -13,18 +16,24 @@ setup(
     version=openpyscad.__version__,
     author=openpyscad.__author__,
     author_email=openpyscad.__email__,
-    description="Python library to generate OpenSCAD source code",
+    description='Python library to generate OpenSCAD source code',
     long_description=long_description,
-    long_description_content_type="text/x-rst",
+    long_description_content_type='text/x-rst',
     url=openpyscad.__url__,
     license=openpyscad.__license__,
-    install_requires=[
-        'six',
-    ],
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    install_requires=requirements,
     classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 3",
-        "Topic :: Software Development :: Code Generators"
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Code Generators'
     ]
 )
