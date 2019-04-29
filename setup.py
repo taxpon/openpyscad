cdb341d8bcfc5576a15b07bf3af17ef52ae3ec31
Takuro Wada <taxpon@gmail.com>
2019-04-29 10:12:00 +0900

    Fix bugs

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import find_packages
 
 import openpyscad
 
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+
 setup(
     name=openpyscad.__name__,
     packages=find_packages(exclude=['tests*', 'example*']),
@@ -10,8 +14,13 @@ setup(
     author=openpyscad.__author__,
     author_email=openpyscad.__email__,
     description="Python library to generate OpenSCAD source code",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     url=openpyscad.__url__,
     license=openpyscad.__license__,
+    install_requires=[
+        'six',
+    ],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2",
